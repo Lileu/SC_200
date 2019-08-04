84b0bdfaa4a0366c5cc7932048698c281d7d7168
Shevi <shevids@gmail.com>
2019-08-04 23:09:45 +1000

    Created fetch requests to API

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,5 +1,21 @@
-$(".heart.fa").click(function() {
-    $(this).toggleClass("fa-heart fa-heart-o");
-  });
-  
-  
+var state = {
+   games : []
+}
+
+$('document').ready(function(){
+  $('.submit-button').on('click', function(e){
+    e.preventDefault();
+
+    var categoryVal = $('#category-selector').val(); 
+    var genreVal = $('#genre-selector').val(); 
+
+    $.ajax({
+      url : `https://steam.cmandersen.com/apps?limit=9&random=1&category=${categoryVal}&genre=${genreVal}&free=0&_=${Date.now()}`, 
+      method : 'GET'
+    })
+    .then((resp) => {
+      console.log(resp)
+    })
+  })
+
+});
